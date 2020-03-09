8238be9611ed93b0cc761217cf01ca3b253cabcf
陈超 <chenchao@moacchina.com>
2020-03-09 17:27:07 +0800

    update typings

--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -1,15 +1,19 @@
-// This file is created by egg-ts-helper@1.25.6
+// This file is created by egg-ts-helper@1.25.7
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+type AnyClass = new (...args: any[]) => any;
+type AnyFunc<T = any> = (...args: any[]) => T;
+type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
+type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
 import ExportChain3 from '../../../app/service/Chain3';
 import ExportTest from '../../../app/service/Test';
 import ExportWeb3 from '../../../app/service/Web3';
 
 declare module 'egg' {
   interface IService {
-    chain3: ExportChain3;
-    test: ExportTest;
-    web3: ExportWeb3;
+    chain3: AutoInstanceType<typeof ExportChain3>;
+    test: AutoInstanceType<typeof ExportTest>;
+    web3: AutoInstanceType<typeof ExportWeb3>;
   }
 }
