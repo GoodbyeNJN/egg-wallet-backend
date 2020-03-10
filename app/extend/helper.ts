c5c16c97c3f923743f136f926b051cc833ab3ed6
陈超 <chenchao@moacchina.com>
2020-03-10 15:26:07 +0800

    add validate method

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -11,4 +11,47 @@ export default {
     hexToUtf8(this: IHelper, hex: string) {
         return web3.utils.hexToUtf8(hex.startsWith("0x") ? hex : `0x${hex}`);
     },
+
+    validateExchange(
+        this: IHelper,
+        rawExchange: {
+            exchangePairId: number;
+            payStatus: "success" | "fail" | "pending";
+            payAddress: string;
+            payValue: bigint;
+            receiveAddress: string;
+            toAddress: string;
+            contractAddress: string | undefined;
+        },
+    ) {
+        // 判断交易是否成功
+        if (rawExchange.payStatus !== "success") {
+            throw Error("Error: Transaction status is not success.");
+        }
+
+        // 判断是否存在交易对id与收款地址
+        if (!rawExchange.exchangePairId || !rawExchange.receiveAddress) {
+            throw Error(
+                "Error: Can not find `exchangePairId` or `receiveAddress` from transaction input data.",
+            );
+        }
+
+        // 判断交易对id是否为数字
+        if (rawExchange.exchangePairId !== Number(rawExchange.exchangePairId)) {
+            throw Error("Error: `exchangePairId` is not a number.");
+        }
+
+        // 判断地址是否合法
+        if (
+            rawExchange.receiveAddress.length !== 42 ||
+            !rawExchange.receiveAddress.startsWith("0x")
+        ) {
+            throw Error("Error: `receiveAddress` is not a valid address.");
+        }
+
+        console.log("queries:", this.ctx.queries);
+        console.log("query:", this.ctx.query);
+
+        return true;
+    },
 };
