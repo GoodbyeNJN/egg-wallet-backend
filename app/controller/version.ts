1ad81772f629e25d14dff6985ae12a291be979f1
陈超 <chenchao@moacchina.com>
2020-03-13 09:35:19 +0800

    add version controller

---
+++ app/controller/version.ts
@@ -0,0 +1,18 @@
+import { Controller } from "egg";
+import { route } from "egg-controller";
+
+export default class VersionController extends Controller {
+    @route("/api/v1/version")
+    public async index() {
+        return {
+            "kaba-wallet": {
+                version: "0.6.2",
+                versionNumber: 602,
+                validVersion: "0.0.0",
+                validVersionNumber: 0,
+                downloadUrl:
+                    "https://moac-wallet.nos-eastchina1.126.net/kaba-wallet/kaba-wallet-latest.apk",
+            },
+        };
+    }
+}
