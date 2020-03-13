ef1b68ce212b8570987dbef0543205f87ece906b
陈超 <chenchao@moacchina.com>
2020-03-13 16:21:46 +0800

    update version controller

--- app/controller/version.ts
+++ app/controller/version.ts
@@ -1,18 +1,65 @@
 import { Controller } from "egg";
-import { route } from "egg-controller";
+import { controller } from "egg-controller";
 
+// 定义新增接口的请求参数规则
+const createRule = {
+    walletName: "string",
+    version: "string",
+    versionNumber: "number",
+    validVersion: "string",
+    validVersionNumber: "number",
+    downloadUrl: "string",
+};
+
+@controller({ name: "Version", prefix: "/api/v1/version", restful: true })
 export default class VersionController extends Controller {
-    @route("/api/v1/version")
+    /**
+     * 查询版本号
+     */
     public async index() {
-        return {
-            "kaba-wallet": {
-                version: "0.6.4",
-                versionNumber: 604,
-                validVersion: "0.0.0",
-                validVersionNumber: 0,
-                downloadUrl:
-                    "https://moac-wallet.nos-eastchina1.126.net/kaba-wallet/kaba-wallet-latest.apk",
-            },
-        };
+        const { app } = this;
+        const versions = await app.model.Version.findAll({
+            attributes: { exclude: ["id", "deleted"] },
+        });
+
+        const versionInfo = {};
+
+        for (const version of versions) {
+            versionInfo[version.walletName] = { ...version.toJSON() };
+        }
+
+        return versionInfo;
+    }
+
+    /**
+     * 修改版本号
+     */
+    public async create(
+        walletName: string,
+        version: string,
+        versionNumber: number,
+        validVersion: string,
+        validVersionNumber: number,
+        downloadUrl: string,
+    ) {
+        const { ctx, app } = this;
+
+        ctx.validate(createRule);
+
+        const result = await app.model.Version.upsert({
+            walletName,
+            version,
+            versionNumber,
+            validVersion,
+            validVersionNumber,
+            downloadUrl,
+        });
+
+        const versions = await app.model.Version.findOne({
+            where: { walletName },
+            attributes: { exclude: ["id", "deleted"] },
+        });
+
+        return versions;
     }
 }
